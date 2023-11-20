b9f02ab63f89635acb9e119265960146c0f3873f
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2023-11-20 09:22:11 -0300

    Implemented a CustomEventArgs section and created a EventArgs for the GameStart event

--- HangmanGame/HangmanGame/CustomEventArgs/GameStartEventArgs.cs
+++ HangmanGame/HangmanGame/CustomEventArgs/GameStartEventArgs.cs
@@ -1,6 +1,11 @@
 ﻿namespace HangmanGame.CustomEventArgs;
 
-public class GameStartEventArgs
+public class GameStartEventArgs : EventArgs
 {
-    
+    public string RandomWord { get; set; }
+
+    public GameStartEventArgs(string word)
+    {
+        RandomWord = word;
+    }
 }
