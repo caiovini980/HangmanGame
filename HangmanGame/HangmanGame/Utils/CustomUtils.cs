e418c9c5179457cb7a9825d69ce904499c82e1e8
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2023-11-23 18:34:22 -0300

    Implemented file utils class

--- HangmanGame/HangmanGame/Utils/CustomUtils.cs
+++ HangmanGame/HangmanGame/Utils/CustomUtils.cs
@@ -1,9 +1,20 @@
 ﻿namespace HangmanGame.Utils;
 
-public static class CustomUtils
+public static class GeneralUtils
 {
     public static int[] FindAllIndexesOf<T>(this IEnumerable<T> values, T item)
     {
         return values.Select((type, i) => Equals(type, item) ? i : -1).Where(i => i != -1).ToArray();
+    }
+}
+
+public static class FileUtils
+{
+    public static string[]? GetSeparatedStrings(string separator, string path)
+    {
+        if (!File.Exists(path)) return null;
+        
+        string figures = File.ReadAllText(path);
+        return figures.Split(separator);
     }
 }
