f0715a2603eae668e818c897927cc4c917911c6a
Caio Vinicius Aguiar Alves <caiovini.aa@gmail.com>
2023-11-30 18:17:34 -0300

    Implemented tests for the Game Manager

--- HangmanGame/HangmanGame/CustomEventArgs/GameStartEventArgs.cs
+++ HangmanGame/HangmanGame/CustomEventArgs/GameStartEventArgs.cs
@@ -6,10 +6,16 @@ public class GameStartEventArgs : EventArgs
 {
     public char[] RandomWord { get; private set; }
     public InputManager InputManagerInstance { get; private set; }
-    
 
+    private string _exceptionMessage = "Given array is empty.\nPlease provide a filled one.";
+    
     public GameStartEventArgs(char[] word, InputManager inputManager)
     {
+        if (word.Length <= 0)
+        {
+            throw new ArgumentException(_exceptionMessage);
+        }
+        
         RandomWord = word;
         InputManagerInstance = inputManager;
     }
